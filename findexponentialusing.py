d9f94cfa33040111c9c466b5f6324cfca86737b9
11249a096-dot <11249a096@kanchiuniv.ac.in>
2025-11-10 15:38:04 +0530

    3

---
+++ findexponentialusing.py
@@ -0,0 +1,14 @@
+# AIM:
+# To compute the exponential value of a number using Python programming.
+
+# ALGORITHM:
+# 1. Import the math module which provides the exp() function.
+# 2. Take a number as input from the user.
+# 3. Use math.exp(x) to calculate e raised to the power of x (i.e., e^x).
+# 4. Display the result.
+
+import math
+
+x = float(input("Enter the exponent value (x): "))
+result = math.exp(x)
+print(f"The exponential of {x} (i.e., e^{x}) is {result}") 
